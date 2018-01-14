d79bd7f525552ee0c17815d479a43eaf310c6368
jessopi <I_jessop@hotmail.com>
2018-01-14 15:34:38 -0500

    Adjusted gameSound, themes were to loud along with SFX except select sound which was too queit

--- Tetris/Tetris/gameSound.cpp
+++ Tetris/Tetris/gameSound.cpp
@@ -6,11 +6,11 @@ gameSound::gameSound(std::string themePath, std::string pausePath, std::string r
 {
 	 theme.openFromFile(themePath);
 	 theme.setLoop(true);
-	 theme.setVolume(20);
+	 theme.setVolume(1);
 
 	 mainMenuTheme.openFromFile("./Audio/menuTheme.ogg");
 	 mainMenuTheme.setLoop(true);
-	 mainMenuTheme.setVolume(40);
+	 mainMenuTheme.setVolume(1);
 
 	 pause.loadFromFile(pausePath);
 	 rotate.loadFromFile(rotatePath);
@@ -40,7 +40,11 @@ gameSound::gameSound(std::string themePath, std::string pausePath, std::string r
  void gameSound::playSFX(std::string trackName)
  {
 	 sound.setBuffer(sfxMap[trackName]);
-	 sound.setVolume(10);
+	 if (trackName == "Select")
+		 sound.setVolume(50);
+	 else
+		sound.setVolume(30);
+
 	 sound.play();
  }
  void gameSound::playTheme()
